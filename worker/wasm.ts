28c9a36863832a3b3d9c165917cf7aaef118f515
Andrew Steinborn <git@steinborn.me>
2020-01-20 17:49:49 -0500

    remove old relict

--- worker/wasm.ts
+++ worker/wasm.ts
@@ -2,7 +2,6 @@
 // See https://github.com/rustwasm/wasm-bindgen/issues/700 for more details.
 export async function getRenderer(): Promise<{
     get_minecraft_head(skin_image: any, size: number): any;
-    blake3(skin_image: any): string;
 }> {
     return new Promise((resolve, reject) => {
         require.ensure([], function () {
