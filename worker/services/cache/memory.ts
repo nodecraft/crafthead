1140c3ea62ddbc1056ae9ea401284519605c06e5
Andrew Steinborn <git@steinborn.me>
2020-02-01 00:32:32 -0500

    Fix erroneous comment

--- worker/services/cache/memory.ts
+++ worker/services/cache/memory.ts
@@ -3,7 +3,7 @@ import LRU from 'lru-cache';
 // MemoryCacheService is an implementation of a cache service that keeps its
 // contents in memory. This is great for fetching recently-retrieved items
 // that happen to land on the same worker. By default, the most recently retrieved
-// 100 items are stored (assuming a generous 4KiB upper bound on responses, this
+// 1024 items are stored (assuming a generous 4KiB upper bound on responses, this
 // is sufficient room for 4MiB worth of responses).
 export default class MemoryCacheService<T> implements CacheService<T> {
     private cache: LRU<string, T>;
