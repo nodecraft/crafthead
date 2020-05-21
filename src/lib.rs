026145928c7dd9974fbc1f5b32a2e9f0a218616c
Andrew Steinborn <git@steinborn.me>
2020-05-21 02:19:53 -0400

    Make the Rust a tiny bit more idiomatic

--- src/lib.rs
+++ src/lib.rs
@@ -19,22 +19,27 @@ cfg_if! {
 }
 
 #[wasm_bindgen]
-pub fn get_minecraft_head(skin_image: Uint8Array, size: u32) -> Uint8Array {
+pub fn get_minecraft_head(skin_image: Uint8Array, size: u32) -> Result<Uint8Array, JsValue> {
     let image_copy = skin_image.to_vec();
 
-    let mut decoded_skin =
-        match image::load_from_memory_with_format(&image_copy, image::ImageFormat::PNG) {
-            Ok(skin) => skin,
-            Err(e) => {
-                let msg = format!("error whilst loading image: {}", e);
-                wasm_bindgen::throw_str(&msg);
-            }
-        };
-    let just_head = decoded_skin.crop(8, 8, 8, 8);
-    let head = just_head.resize(size, size, image::imageops::FilterType::Nearest);
+    let skin_result = image::load_from_memory_with_format(&image_copy, image::ImageFormat::Png);
+    match skin_result {
+        Ok(skin) => {
+            let head = crop_head(skin, size);
+            let mut result = Vec::with_capacity(1024);
+            return match head.write_to(&mut result, image::ImageFormat::Png) {
+                Ok(()) => Ok(Uint8Array::from(&result[..])),
+                Err(_err) => Err(js_sys::Error::new("Couldn't save resized skin.").into())
+            };
+        },
+        Err(_err) => {
+            return Err(js_sys::Error::new("Couldn't load skin.").into());
+        }
+    }
+}
 
-    // For "common" image sizes, 1KiB should be plenty (typically < 140px)
-    let mut result = Vec::with_capacity(1024);
-    head.write_to(&mut result, image::ImageFormat::PNG).unwrap();
-    return Uint8Array::from(&result[..]);
+fn crop_head(skin: image::DynamicImage, size: u32) -> image::DynamicImage {
+    let just_head = skin.crop_imm(8, 8, 8, 8);
+    let head = just_head.resize(size, size, image::imageops::FilterType::Nearest);
+    head
 }
