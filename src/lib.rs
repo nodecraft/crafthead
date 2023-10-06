3f3a6ad0be35d8de296f530e5b1af941ca7e81da
James Ross <james@jross.me>
2023-10-06 22:36:59 +0100

    chore: remove wee-alloc
    This is no longer maintained and has outstanding memory leak and security issues.

--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,6 @@ use wasm_bindgen::prelude::*;
 use skin::*;
 use image::DynamicImage;
 
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
-
 enum RenderType {
     Avatar,
     Helm,
